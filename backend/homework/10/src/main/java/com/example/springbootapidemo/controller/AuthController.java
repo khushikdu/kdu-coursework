2081c5b72a9e2da7be094566a56c59bd529b7931
kdukhushi <khushi.rani@kickdrumtech.com>
2024-02-03 01:02:39 +0530

    Removing smells for homework-10

--- backend/homework/10/src/main/java/com/example/springbootapidemo/controller/AuthController.java
+++ backend/homework/10/src/main/java/com/example/springbootapidemo/controller/AuthController.java
@@ -1,12 +1,9 @@
 package com.example.springbootapidemo.controller;
 
 
-import com.example.springbootapidemo.dto.AuthDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
