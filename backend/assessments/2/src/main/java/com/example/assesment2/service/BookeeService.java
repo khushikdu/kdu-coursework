36332695244ba05f6bfed61d83772dc484a98e52
kdukhushi <khushi.rani@kickdrumtech.com>
2024-02-08 16:51:43 +0530

    Removing smells from assesment-2

--- backend/assessments/2/src/main/java/com/example/assesment2/service/BookeeService.java
+++ backend/assessments/2/src/main/java/com/example/assesment2/service/BookeeService.java
@@ -1,26 +1,18 @@
 package com.example.assesment2.service;
 
 import com.example.assesment2.entity.Bookee;
-import com.example.assesment2.entity.User;
 import com.example.assesment2.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookeeService{
-    private BookeeRepository bookeeRepository;
-    private EventRepository eventRepository;
-    private CatalogRepository catalogRepository;
-    private TransactionRepository transactionRepository;
+    private final BookeeRepository bookeeRepository;
     @Autowired
-    public BookeeService(BookeeRepository bookeeRepository, EventRepository eventRepository, CatalogRepository catalogRepository, TransactionRepository transactionRepository) {
+    public BookeeService(BookeeRepository bookeeRepository) {
         this.bookeeRepository = bookeeRepository;
-        this.eventRepository = eventRepository;
-        this.catalogRepository = catalogRepository;
-        this.transactionRepository = transactionRepository;
     }
     public List<Bookee> findAll() {
         return bookeeRepository.findAll();
