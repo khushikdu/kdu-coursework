31867e9b08c566f4cd7b459b839a4874e17b2210
kdukhushi <khushi.rani@kickdrumtech.com>
2024-02-13 22:34:47 +0530

    Adding files for homework-6

---
+++ frontend/homework/6/data.js
@@ -0,0 +1,19 @@
+let accounts = [
+    {
+      "id": 1,
+      "username": "sasi",
+      "position": "manager"
+    },
+    {
+      "id": 2,
+      "username": "sanskar",
+      "position": "guest"
+    },
+    {
+      "id": 3,
+      "username": "tanya",
+      "position": "guest"
+    }
+];
+
+module.exports=accounts
