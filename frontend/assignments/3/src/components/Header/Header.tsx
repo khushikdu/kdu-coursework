d332c5080e80ba222f5442d8d590bd1746f9b764
kdukhushi <khushi.rani@kickdrumtech.com>
2024-03-03 20:56:14 +0530

    FE Mini Project: Initial Commit

---
+++ frontend/assignments/3/src/components/Header/Header.tsx
@@ -0,0 +1,45 @@
+import ShowChartIcon from "@mui/icons-material/ShowChart";
+import { styles } from "./Header.styles";
+import { Link } from "react-router-dom";
+import AllStockComponents from "../Dasboard/AllStocksComponent/AllStockComponent";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useEffect } from "react";
+import { stockthunk } from "../../redux/thunk/stockThunk";
+
+function Header() {
+  const dispatch = useDispatch();
+  const stocks = useSelector((state: RootState) => state.stocks.stockList);
+
+  useEffect(() => {
+    dispatch(stockthunk());
+  }, [dispatch]);
+
+  // console.log("in header",stocks)
+
+  return (
+    <div className="main" style={styles.main}>
+      <div className="main" style={styles.mainHeader}>
+        <div className="left-logo-title" style={styles.leftLogoTitle}>
+          <Link to={`/`} style={styles.linkStyle}>
+            <ShowChartIcon />
+          </Link>
+          KDU Stock Market
+        </div>
+
+        <div className="right-title" style={styles.rigthTitle}>
+          <div className="summariser" style={styles.summariser}>
+            Summarizer
+          </div>
+          <div className="portfolio">
+            <Link to={`/portfolio`} style={styles.linkStyle}>
+              My Portfolio
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
